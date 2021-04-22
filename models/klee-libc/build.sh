97eceb998bf425b7c218147e6fd87a73aea2b0fb
David Korczynski <david@adalogics.com>
2021-04-22 11:03:26 +0100

    Added a models directory.

---
+++ models/klee-libc/build.sh
@@ -0,0 +1,9 @@
+CS="atexit.c atoi.c bcmp.c calloc.c htonl.c memchr.c mempcpy.c putchar.c stpcpy.c strcat.c strchr.c strcmp.c strcoll.c strcpy.c strlen.c strncmp.c strncpy.c strrchr.c strtol.c strtoul.c tolower.c toupper.c"
+
+for f in ${CS}
+do
+	echo ${f}
+	SYMCC_REGULAR_LIBCXX=1 symcc -c ${f} -o ${f}.o
+done
+
+ar -cr minilibc.a *.o 
