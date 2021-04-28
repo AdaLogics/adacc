d37640d5ced00e7693fb0dab9fceee78aa990dfb
David Korczynski <david@adalogics.com>
2021-04-28 15:53:08 +0100

    adapted examples to new set up.

--- examples/build_fuzzer.sh
+++ examples/build_fuzzer.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/nodejs/http-parser
 cd http-parser
-SYMCC_REGULAR_LIBCXX=1 sym++ -c -I./ ./fuzzers/fuzz_url.c -o fuzz_url.o 
-SYMCC_REGULAR_LIBCXX=1 sym++ -c -I./ ./http_parser.c -o http_parser.o 
+SYMCC_REGULAR_LIBCXX=1 symcc -fsanitize-coverage=inline-8bit-counters -c -I./ ./fuzzers/fuzz_url.c -o fuzz_url.o 
+SYMCC_REGULAR_LIBCXX=1 symcc -fsanitize-coverage=inline-8bit-counters -c -I./ ./http_parser.c -o http_parser.o 
 cd ../
-SYMCC_REGULAR_LIBCXX=1 sym++ libfuzz-harness-proxy.c ./http-parser/fuzz_url.o ./http-parser/http_parser.o -o symcc-http-parse-fuzz-url
+SYMCC_REGULAR_LIBCXX=1 symcc libfuzz-harness-proxy.c -fsanitize-coverage=inline-8bit-counters ./http-parser/fuzz_url.o ./http-parser/http_parser.o ../models/klee-libc/minilibc.a -o symcc-http-parse-fuzz-url 
