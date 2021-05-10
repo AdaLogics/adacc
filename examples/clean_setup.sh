a7b0878eaa5e78f1ea91f4309be115b0f39ebd4e
David Korczynski <david@adalogics.com>
2021-05-10 19:25:27 +0100

    added helper script.

---
+++ examples/clean_setup.sh
@@ -0,0 +1,12 @@
+rm ./corpus_counters.stats
+touch ./corpus_counters.stats
+
+rm ./prev_1s.txt
+touch ./prev_1s.txt
+
+rm ./prev_0s.txt
+touch ./prev_0s.txt
+
+rm ./trace_maps.txt
+touch ./trace_maps.txt
+
