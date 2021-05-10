18997cfdb33783c4818ea702beefef26d089ee2a
David Korczynski <david@adalogics.com>
2021-05-10 19:24:43 +0100

    Build for simple statemachines.

--- examples/build_simple_samples.sh
+++ examples/build_simple_samples.sh
@@ -6,7 +6,12 @@
 #SYMCC_REGULAR_LIBCXX=1 sym++ ./symcc-issue-23.c ../models/klee-libc/minilibc.a -o symcc-issue-23-example
 
 
+
 SYMCC_REGULAR_LIBCXX=1 symcc ./simple-example.c -o simple-example
+SYMCC_REGULAR_LIBCXX=1 symcc ./simple-switch.c -o simple-switch
 
-
+SYMCC_REGULAR_LIBCXX=1 symcc ./simple-statemachine.c -o simple-statemachine
+SYMCC_REGULAR_LIBCXX=1 symcc ./simple-statemachine2.c -o simple-statemachine2
+SYMCC_REGULAR_LIBCXX=1 symcc ./simple-statemachine3.c -o simple-statemachine3
+SYMCC_REGULAR_LIBCXX=1 symcc ./simple-statemachine4.c -o simple-statemachine4
 #sym++ ./hello-world.cpp -o hello-world
