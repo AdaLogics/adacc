4d85a63203d83ea486da083c3af62b8b2c8e2b17
David Korczynski <david@adalogics.com>
2021-05-11 17:32:51 +0100

    cleanup.

--- examples/simple-statemachine2.c
+++ examples/simple-statemachine2.c
@@ -3,40 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-/*
-#ifndef NOCOV
-extern void __s2anitizer_cov_trace_pc_guard(uint32_t *guard);
-extern void __s2anitizer_cov_trace_pc_guard_init(uint32_t *start, uint32_t *stop);
-
-void __sanitizer_cov_trace_pc_guard_init(uint32_t *start,
-                                                            uint32_t *stop) {
-        printf("Hulla hop from init\n");
-        __s2anitizer_cov_trace_pc_guard_init(start, stop);
-        printf("dooooooooone\n");
-}
-
-void __sanitizer_cov_trace_pc_guard(uint32_t *guard) {
-   printf("Calling into s2\n");
-   __s2anitizer_cov_trace_pc_guard(guard);
-   printf("done\n");
-}
-
-extern void __s2anitizer_cov_8bit_counters_init(char *start, char *end);
-
-void __sanitizer_cov_8bit_counters_init(char *start, char *end) {
-  // [start,end) is the array of 8-bit counters created for the current DSO.
-  // printf("Yup yey\n");
-  // Capture this array in order to read/modify the counters.
-  char *tmp_p = start;
-  while (tmp_p < end) {
-    char c = *tmp_p;
-    printf("Counter value: %d\n", (int)c);
-    tmp_p++;
-  }
-  __s2anitizer_cov_8bit_counters_init(start, end);
-}
-#endif
-*/
 
 void ps(int state) {
         if (state == 1) {
