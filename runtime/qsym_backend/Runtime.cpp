81a588508530ace7fe59540b92b747042b5f8600
David Korczynski <david@adalogics.com>
2021-05-26 17:44:39 +0100

    fixed llvm style printing

--- runtime/qsym_backend/Runtime.cpp
+++ runtime/qsym_backend/Runtime.cpp
@@ -618,7 +618,7 @@ bool pure_concolic_should_save(SymExpr constraint, int taken,
 
 void _sym_push_path_constraint(SymExpr constraint, int taken,
                                uintptr_t site_id) {
-  errs() << "pushing path constraint\n";
+  std::cerr << "pushing path constraint\n";
   if (constraint == nullptr)
     return;
 
@@ -694,7 +694,7 @@ UNSUPPORTED(SymExpr _sym_build_float_to_unsigned_integer(SymExpr, uint8_t))
 static int idx_hop = 0;
 
 void _symcc_cov_cb(uint32_t cb_id) {
-errs() << "symcc_cov_cb\n":
+std::cerr << "symcc_cov_cb\n":
     if (counter_map.count(cb_id) == 0) {
         // Insert the counter in the map.
         counter_map[cb_id] = 1;
@@ -762,10 +762,10 @@ bool _sym_feasible(SymExpr expr) {
 //
 
 void _sym_collect_garbage() {
-  errs() << "collect garbage 1\n";
+  std::cerr << "collect garbage 1\n";
   if (allocatedExpressions.size() < g_config.garbageCollectionThreshold)
     return;
-  errs() << "collect garbage 2\n";
+  std::cerr << "collect garbage 2\n";
 
 #ifdef DEBUG_RUNTIME
   auto start = std::chrono::high_resolution_clock::now();
@@ -780,7 +780,7 @@ void _sym_collect_garbage() {
       ++expr_it;
     }
   }
-  errs() << "collect garbage 3\n";
+  std::cerr << "collect garbage 3\n";
 
 #ifdef DEBUG_RUNTIME
   auto end = std::chrono::high_resolution_clock::now();
@@ -793,5 +793,5 @@ void _sym_collect_garbage() {
                    .count()
             << " milliseconds)" << std::endl;
 #endif
-  errs() << "collect garbage 4\n";
+  std::cerr << "collect garbage 4\n";
 }
